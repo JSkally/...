bca562304254b46018b3de0094c1d3ae0a940885
Jay Skally <@J_Skally>
2012-11-05 21:56:19 -0500

    THE GUI :D

---
+++ Fuck the Weather/Fuck the Weather/Form4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twitterizer.Core;
+
+ 
+
+namespace Fuck_the_Weather
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+  
+
+            Form5 f5 = new Form5();
+            f5.Show();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
